3988c2975a19cf4645167ef257384690c49d2f94
Mohan Kumar <Mohan.Kumar@exponential.com>
2018-10-31 17:24:24 +0530

    Updates

---
+++ precache-manifest.fcdadf3d00cbe682bd8bb8ad4d08b20c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "089928e318ee73d13ddb",
+    "url": "/progressBar/static/js/runtime~main.089928e3.js"
+  },
+  {
+    "revision": "ce18bf35d3b706c40511",
+    "url": "/progressBar/static/js/main.ce18bf35.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/progressBar/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "ce18bf35d3b706c40511",
+    "url": "/progressBar/static/css/main.7c6f8b53.chunk.css"
+  },
+  {
+    "revision": "4c2f70d8aaed844eeacefda16c206626",
+    "url": "/progressBar/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ProgressBarExample from './ProgressBarExample';
-
-ReactDOM.render(<ProgressBarExample />, document.getElementById('root'));
